a985a07b4fe011d04947dc176b43dce48ead3676
Glenn Hamilton-Smith <glenniumhs@gmail.com>
2023-03-26 23:00:17 +0100

    Minor tweaks for non-touch devices on blog/[slug]

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -7,9 +7,26 @@
       return queryContent().where({ slug: postSlug }).findOne()
     } else { return { "error" : true } }
   })
+  const hasHover = ref(true);
 
   onMounted(() => {
-    document.querySelector('')
+    if(window.matchMedia("(any-hover: none)").matches) {
+        hasHover.value = false
+        setTimeout(() => document.querySelector('.click-hint').style.opacity = 0, 3000)
+    }
+    
+    let hoverTimer;
+    const elImage = document.getElementById('post-image')
+    elImage.addEventListener('mouseenter', () => {
+      hoverTimer = setTimeout(()=> {
+        elImage.classList.add('hover')
+        document.querySelector('.click-hint').style.display = 'none'
+      }, 1000)
+    })
+    elImage.addEventListener('mouseleave', () => {
+      elImage.classList.remove('hover')
+      if(hoverTimer) clearTimeout(hoverTimer)
+    })
   })
 </script>
 
@@ -18,8 +35,8 @@
     <a class="text-slate-200 hover:text-slate-400 transition cursor-pointer font-bold text-md" onclick="window.history.go(-1)">&lt;&lt; Go Back</a>
     <div class="flex flex-col items-start gap-y-4">
       <NuxtLink v-if="blogPost.image" :to="blogPost.image" class="w-full flex items-center justify-center" target="_blank">
-        <img :src="blogPost.image" class="object-cover w-full max-h-96"/>
-        <span class="click-hint hidden lg:block absolute z-10 bg-slate-900 bg-opacity-20 px-2 py-5 rounded-2xl">Hover to expand | Click to see full image</span>
+        <img :src="blogPost.image" id="post-image" class="object-cover w-full max-h-96"/>
+        <span class="click-hint absolute z-10 bg-slate-900 bg-opacity-40 px-2 py-5 rounded-2xl transition">{{ hasHover ? 'Hover to expand | ' : ''  }}Click to see full image</span>
       </NuxtLink>
       <h1 class="text-3xl font-bold">{{ blogPost.title }}</h1>
       <p class="italic">{{ blogPost.excerpt }}</p>
@@ -35,10 +52,11 @@
 
 <style scoped>
   img {
-    transition: max-height 1.6s ease-in-out;
+    transition: max-height 1.5s ease-out;
   }
 
   .blog-post img.hover {
+    transition: max-height 3.5s ease-out; /* Basically, the hover delay gets real quirky. Don't think too hard */
     max-height: 10000px;
   }
 
