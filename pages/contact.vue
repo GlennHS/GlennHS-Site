4beb658074dfb3dd190336e6442c8f25812f1697
Glenn Hamilton-Smith <glenniumhs@gmail.com>
2023-04-11 00:25:57 +0100

    Updated opacity on parallax filters, looks brighter now.

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header id="parallax-1" class="relative w-full h-full flex items-center justify-center parallax">
-      <div class="absolute top-0 left-0 w-full h-full bg-slate-600 bg-opacity-60 flex flex-col items-baseline justify-center">
+      <div class="absolute top-0 left-0 w-full h-full bg-slate-600 bg-opacity-20 flex flex-col items-baseline justify-center">
         <div ref="heroContent" class="backdrop-blur-md bg-gray-900 bg-opacity-50 rounded-r-lg w-full px-4 lg:px-20 py-4 lg:py-10 flex flex-col justify-center items-baseline gap-y-6 max-w-sm md:max-w-xl lg:max-w-2xl xl:max-w-3xl">
           <h1 class="text-lg sm:text-xl md:text-2xl xl:text-4xl font-bold">Contact Me.</h1>
           <span class="text-base md:text-lg italic font-semibold">Carrier pigeon has been discontinued, sorry.</span>
--- pages/cv.vue
+++ pages/cv.vue
@@ -23,7 +23,7 @@
 <template>
   <div>
     <header id="parallax-1" class="relative w-full h-full flex items-center justify-center parallax">
-      <div class="absolute top-0 left-0 w-full h-full bg-slate-600 bg-opacity-60 flex flex-col items-baseline justify-center">
+      <div class="absolute top-0 left-0 w-full h-full bg-slate-600 bg-opacity-20 flex flex-col items-baseline justify-center">
         <div ref="heroContent" class="slide-in-left backdrop-blur-md bg-gray-900 bg-opacity-50 rounded-r-lg w-full px-4 lg:px-20 py-4 lg:py-10 flex flex-col justify-center items-baseline gap-y-6 max-w-lg lg:max-w-xl xl:max-w-3xl mt-32 md:mt-0">
           <h1 class="text-lg md:text-xl lg:text-2xl xl:text-4xl font-bold">Digital CV.</h1>
           <span class="text-base lg:text-lg italic font-semibold">In a hurry? Need a different format? 
