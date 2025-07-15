b76d96d99b5988fc281aeb5ce15634ee94148d3e
GlennHS <glenniumhs@gmail.com>
2025-07-15 01:20:42 +0100

    Update hire sticky

--- components/HireSticky.vue
+++ components/HireSticky.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="w-full bg-red-600 text-white text-center italic text-bold text-lg py-1 z-50">
-    <span>Available for hire! Shoot me a message on
+    <span>Available for full-time hire from 20th August! Shoot me a message on
       <a class="underline" target="_blank" href="https://linkedin.com/in/glennhs">LinkedIn</a>
       or <a class="underline" target="_blank" href="mailto:glenniumhs@gmail.com">email</a>
     </span>
